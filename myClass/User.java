4e0d0076fa7a5b899349fd94939d261410b05fe5
soma <purguy3@gmail.com>
2025-10-18 19:46:37 +0900

    푸시

--- myClass/User.java
+++ myClass/User.java
@@ -1,6 +1,7 @@
 package myClass;
+
 /**
- * User는 학생객체를 생성하는 클래스이다
+ * User는 학생 객체를 생성하는 클래스이다.
  *
  * @author (2021320090 이봉헌, 2021320018 김준혁, 2024320003 니시 야스히로)
  * @version (2025.10.19)
@@ -8,23 +9,33 @@ package myClass;
 public class User extends DB_Element {
     private String name;
     private Integer stID;
-    
-    // 입력값 : 이용자의 아이디(stID), 이용자의 이름(name)
-    // 반환값 : 없음
-    public User(int stID, String name){
+
+    /**
+     * User 생성자 - 이용자의 ID와 이름을 초기화한다.
+     *
+     * @param stID  이용자의 (ID)
+     * @param name  이용자의 이름
+     */
+    public User(int stID, String name) {
         this.name = name;
         this.stID = stID;
     }
-    
-    // 입력값 : 없음
-    // 반환값 : 이용자 ID에 대한 문자열
+
+    /**
+     * getID 메소드 - 이용자의 ID를 문자열로 반환한다.
+     *
+     * @return  이용자의 ID 문자열
+     */
     @Override
-    public String getID() { 
-        return this.stID.toString(); 
+    public String getID() {
+        return this.stID.toString();
     }
 
-    // 입력값 : 없음
-    // 반환값 : 이용자에 대한 정보
+    /**
+     * toString 메소드 - 이용자의 정보를 문자열로 반환한다.
+     *
+     * @return  이용자에 대한 정보
+     */
     @Override
     public String toString() {
         return "[" + this.stID + "] " + this.name;
